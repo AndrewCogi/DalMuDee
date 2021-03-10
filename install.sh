f231c542845525940212a9ab681706bf420bc8dc
CHOHYUNSIK <whgustlr0326@gmail.com>
2021-03-10 11:22:55 +0000

    change jnvim->jnvim2.0

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 set -o nounset		#미선언 변수 접근 시 에러처리
 set -o errexit		#커맨드 실패하면 바로 중지
 
-echo "Welcome to JNvim"
-echo "Start Installing JNvim..."
+echo "Welcome to JNvim2.0"
+echo "Start Installing JNvim2.0..."
 sudo apt update
 sudo apt upgrade -y
 sudo apt autoremove -y
@@ -13,5 +13,5 @@ echo "Install neovim 5.0..."
 sudo add-apt-repository ppa:neovim-ppa/unstable -y
 sudo apt install neovim -y
 
-echo "Cloning JNvim Configuration..."
+echo "Cloning JNvim2.0 Configuration..."
 git clone https://github.com/CHOHYUNSIK/JNvim2.0.git ~/.config/nvim
