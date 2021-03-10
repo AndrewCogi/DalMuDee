3b9873b0d5af8183fbc7005659111160790e4e5d
CHOHYUNSIK <whgustlr0326@gmail.com>
2021-03-10 10:45:34 +0000

    add install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -o nounset		#미선언 변수 접근 시 에러처리
+set -o errexit		#커맨드 실패하면 바로 중지
+
+echo "Welcome to JNvim"
+echo "Start Installing JNvim..."
+sudo apt update
+sudo apt upgrade -y
+sudo apt autoremove -y
+
+echo "Install neovim 5.0..."
+sudo add-apt-repository ppa:neovim-ppa/unstable -y
+sudo apt install neovim -y
+
