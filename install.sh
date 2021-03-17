db2d2c1b8293ec4eab36c7481c7cbfd3df350041
CHOHYUNSIK <whgustlr0326@gmail.com>
2021-03-17 11:48:45 +0000

    modify install.sh (add install node,pip,pynvim etc..)

--- install.sh
+++ install.sh
@@ -3,7 +3,56 @@
 set -o nounset		#미선언 변수 접근 시 에러처리
 set -o errexit		#커맨드 실패하면 바로 중지
 
+installnodeubuntu() { \
+  sudo apt install nodejs -y
+  sudo apt install npm -y
+}
+
+installpiponubuntu() { \
+  sudo apt install python3-pip -y # > /dev/null
+}
+
+installnode() { \
+  echo "Installing node..."
+  [  -n "$(uname -a | grep Ubuntu)" ] && installnodeubuntu
+  sudo npm i -g neovim
+}
+
+installpip() { \
+  echo "Installing pip..."
+  [  -n "$(uname -a | grep Ubuntu)" ] && installpiponubuntu
+}
+
+asktoinstallnode() { \
+  echo "node not found"
+  echo -n "Would you like to install node now (y/n)? "
+  read answer
+  [ "$answer" != "${answer#[Yy]}" ] && installnode && installcocextensions
+}
+
+asktoinstallpip() { \
+  echo "pip not found"
+  echo -n "Would you like to install pip now (y/n)? "
+  read answer
+  [ "$answer" != "${answer#[Yy]}" ] && installpip
+}
+
+installpynvim() { \
+  echo "Installing pynvim..."
+  pip3 install pynvim --user
+}
+
+# install pynvim
+pip3 list | grep pynvim > /dev/null && echo "pynvim installed, moving on..." || installpynvim
+
 echo "Welcome to JNvim2.0"
+
+# Installing pip
+which pip3 > /dev/null && echo "pip installed, moving on..." || asktoinstallpip
+
+# install node and neovim support
+which node > /dev/null && echo "node installed, moving on..." || asktoinstallnode
+
 echo "Start Installing JNvim2.0..."
 sudo apt update
 sudo apt upgrade -y
